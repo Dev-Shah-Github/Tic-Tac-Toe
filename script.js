a47da4ef058cca917d19570c8fa9ea3a751f7fe6
Dev-Shah-Github <123110679+Dev-Shah-Github@users.noreply.github.com>
2024-07-06 11:21:23 +0530

    mode activation added

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-let board;
+let game_mode = "two-player-mode";
+let player_1 = ""
+
+function select_game_mode(mode){
+    if(mode.value=="ai-mode"){
+        game_mode = "ai-mode";
+    }
+    else if(mode.value=="two-player-mode"){
+        game_mode = "two-player-mode";
+    }
+
+    let modes = document.getElementsByClassName("mode-button");
+    for(let i = 0 ;i<modes.length;i++){
+        modes[i].classList.remove("mode-selected");
+    }
+    mode.classList.add("mode-selected");
+
+    start_game();
+}
+
+function start_game(){
+    const board = [[" "," "," "],[" "," "," "],[" "," "," "]];
+    let winner = "?";
+}
