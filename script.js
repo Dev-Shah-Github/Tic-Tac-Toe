878eb72b9f9d9ac62b2081cb53e6eb69d07b5396
Dev-Shah-Github <123110679+Dev-Shah-Github@users.noreply.github.com>
2024-07-10 19:16:30 +0530

    Major update in script.js + manor update in HTML  & CSS
    add some working mechanism like:
    1) update game board on each move
    2) update live player pointer feature added
    3) update some variables
    4) created the game restart function
    5) create function to perform move by player
    6) player name for the user feature added
    etc.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let game_mode = "two-player-mode";
-let player_1 = ""
+const O_src = "./images/circle.png";
+const X_src = "./images/cross.png";
+
+let player_1 = {name:"",symbol:"O"}
+let player_2 = {name:"",symbol:"X"}
+let encountered_moves = [];
+let board = [[" "," "," "],[" "," "," "],[" "," "," "]];
+let winner = "?";
+
+document.getElementById('pointer-p1').style.opacity = 1;
+document.getElementById('pointer-p2').style.opacity = 0;
 
 function select_game_mode(mode){
     if(mode.value=="ai-mode"){
@@ -15,10 +25,104 @@ function select_game_mode(mode){
     }
     mode.classList.add("mode-selected");
 
-    start_game();
+    assgin_players();
 }
 
-function start_game(){
-    const board = [[" "," "," "],[" "," "," "],[" "," "," "]];
-    let winner = "?";
+// for assign name to players
+function assgin_players(){
+    player_1.name = prompt("Enter Name of Player-1.");
+    if(game_mode=="two-player-mode"){
+        player_2.name = prompt("Enter Name of Player-2.");
+    }
+    else if(game_mode=="ai-mode"){
+        player_2.name = "ai";
+    }
+}
+
+// player perform move 
+function player_move(click){
+    let clicked_box = click.id.replace("box","");
+    let i = parseInt(clicked_box[0]);
+    let j = parseInt(clicked_box[1]);
+    console.log(clicked_box);
+
+    let move_number = encountered_moves.length+1;
+
+    if(move_number!=10){
+        if(!encountered_moves.includes(clicked_box)){
+            encountered_moves.push(clicked_box);
+            if(move_number%2!=0){
+                board[i][j]=player_1.symbol;
+            }
+            else{
+                board[i][j]=player_2.symbol;
+            }
+            update_board()
+            update_pointer(move_number)
+        }
+        else{
+            console.log("this move already encountered!");
+        }
+    }
+    else{
+        console.log("game over!");
+    }
+}
+
+// function symbol(move_num){
+//     if(move_num%2!=0){
+        
+//     }
+//     else{
+
+//     }
+// }
+
+// updating the live player pointer
+function update_pointer(move_number){
+    if(move_number!=9){
+        if(move_number%2==0){
+            document.getElementById('pointer-p1').style.opacity = 1;
+            document.getElementById('pointer-p2').style.opacity = 0;
+        }
+        else{
+            document.getElementById('pointer-p1').style.opacity = 0;
+            document.getElementById('pointer-p2').style.opacity = 1;
+        }
+    }
+    else{
+        document.getElementById('pointer-p1').style.opacity = 0;
+        document.getElementById('pointer-p2').style.opacity = 0;
+        document.getElementById("game-status").innerHTML = "🚩 GAME OVER! 🏁";
+    }
+    
+}
+
+// updating the O-X symbols on the board.
+function update_board(){
+    for(let i = 0; i<3 ; i++){
+        for(let j = 0; j<3; j++){
+            if(board[i][j]=="O"){
+                document.getElementById(`img${i}${j}`).src = O_src;
+            }
+            else if(board[i][j]=="X"){
+                document.getElementById(`img${i}${j}`).src = X_src;
+            }
+        }
+    }
+}
+
+function restart_game(){
+    player_1 = {name:"",symbol:"O"}
+    player_2 = {name:"",symbol:"X"}
+    encountered_moves = [];
+    board = [[" "," "," "],[" "," "," "],[" "," "," "]];
+    winner = "?";
+    document.getElementById('pointer-p1').style.opacity = 1;
+    document.getElementById('pointer-p2').style.opacity = 0;
+    for(let i = 0; i<3 ; i++){
+        for(let j = 0; j<3; j++){
+            document.getElementById(`img${i}${j}`).src = "./images/blank.png";
+        }
+    }
 }
